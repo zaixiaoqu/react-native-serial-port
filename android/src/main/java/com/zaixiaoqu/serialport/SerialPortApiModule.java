b49b0632d7c67d3271acbca79c22f8877b8c7e12
余胜 <email@sample.com>
2021-11-21 00:52:05 +0800

    import androidx.annotation.Nullable;

--- android/src/main/java/com/zaixiaoqu/serialport/SerialPortApiModule.java
+++ android/src/main/java/com/zaixiaoqu/serialport/SerialPortApiModule.java
@@ -2,7 +2,7 @@ package com.zaixiaoqu.serialport;
 
 import android.serialport.SerialPort;
 import android.serialport.SerialPortFinder;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
